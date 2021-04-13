6d70d8f88cca22dc67993034ed1bcf55a867e35c
sugarnyang <midjhincarry@gmail.com>
2021-04-13 23:44:08 +0900

    Arrays.sort

--- src/test/java/javaAlgorithm/ApplicationTests.java
+++ src/test/java/javaAlgorithm/ApplicationTests.java
@@ -1,13 +1,27 @@
 package javaAlgorithm;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Arrays;
+
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+		String[] completion = {"josipa", "filipa", "marina", "nikola"};
+
+		System.out.println(solution(participant, completion));
 	}
 
+	public String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		for (int i = 0; i < completion.length; i++) {
+			if (!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+		return participant[completion.length];
+	}
 }
